7b3572577f843f284e3599090878c03da439ba1e
Magdalena <164169718+MagdalenaSmls@users.noreply.github.com>
2024-06-06 08:31:15 +0000

    test

--- stack.py
+++ stack.py
@@ -1,20 +1,31 @@
 class Stack:
     """Provides stack container"""
-    def __init__(self):
-        pass
+	def __init__(self, typ = None):
+		self.items = []
+		self.typ = typ
 
-    def push(self, data):
-        pass
+	def push(self, item):
+		if isinstance(type, type(self.typ)) == False:
+			if isinstance(item , type(self.typ)) == False:
+				raise TypeError
+		else:
+			if isinstance(item , self.typ) == False:
+				raise TypeError
+		self.items.insert(0 , item)
 
-    def pop(self):
-        pass
+	def pop(self):
+		if len(self.items) == 0:
+			return None
+		return self.items.pop(0)
+	
+	def get(self):
+		if len(self.items) == 0:
+			return None
+		return self.items[-1]
+		
 
-    def get(self):
-        pass
-
-    def length(self):
-        pass
-
+	def length(self):
+		return len(self.items)
     def __str__(self):
         pass
 
