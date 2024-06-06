df84aaade05a694f9824cce35e76a29d36c42054
Magdalena <164169718+MagdalenaSmls@users.noreply.github.com>
2024-06-06 08:43:09 +0000

    poprawka

--- stack.py
+++ stack.py
@@ -1,31 +1,34 @@
+"""CLASS STACK"""
+
+
 class Stack:
     """Provides stack container"""
-	def __init__(self, typ = None):
-		self.items = []
-		self.typ = typ
+    def __init__(self, typ=None):
+        self.items = []
+        self.typ = typ
 
-	def push(self, item):
-		if isinstance(type, type(self.typ)) == False:
-			if isinstance(item , type(self.typ)) == False:
-				raise TypeError
-		else:
-			if isinstance(item , self.typ) == False:
-				raise TypeError
-		self.items.insert(0 , item)
+    def push(self, item):
+        if isinstance(type, type(self.typ)) is False:
+            if isinstance(item, type(self.typ)) is False:
+                raise TypeError
+        else:
+            if isinstance(item, self.typ) is False:
+                raise TypeError
+        self.items.insert(0, item)
 
-	def pop(self):
-		if len(self.items) == 0:
-			return None
-		return self.items.pop(0)
-	
-	def get(self):
-		if len(self.items) == 0:
-			return None
-		return self.items[-1]
-		
+    def pop(self):
+        if len(self.items) == 0:
+            return None
+        return self.items.pop(0)
 
-	def length(self):
-		return len(self.items)
+    def get(self):
+        if len(self.items) == 0:
+            return None
+        return self.items[-1]
+
+    def length(self):
+        return len(self.items)
+
     def __str__(self):
         pass
 
